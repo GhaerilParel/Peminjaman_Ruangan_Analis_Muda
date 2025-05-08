81b51505846056bd584054502a200e45a4f78e6d
GhaerilJuniawan <EmailKamu>
2025-05-08 20:23:07 +0700

    revisi 10

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,6 @@ Route::middleware(['auth:web'])->group(function () {
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 
 Route::get('/api/rooms/recommendation', [RoomController::class, 'getRecommendations'])->name('api.rooms.recommendation');
+
+Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/admin/login', [AuthController::class, 'login'])->name('admin.login.process');
